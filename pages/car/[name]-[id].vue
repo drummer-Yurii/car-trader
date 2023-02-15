9b46ee94c35b4258d4ad4e954389870cf6555c7b
Yurii Rybachok <drummer2009@ukr.net>
2023-02-15 17:49:08 +0200

    custom layout for car page

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -4,10 +4,14 @@ useHead({
     title: toTitleCase(route.params.name)
 });
 
+definePageMeta({
+    layout: "custom"
+})
+
 function toTitleCase(str) {
     return str.replace(
         /\w\S*/g,
-        function(txt) {
+        function (txt) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }
     );
@@ -16,11 +20,9 @@ function toTitleCase(str) {
 
 <template>
     <div>
-        <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-            <CarDetailHero />
-            <CarDetailAttributes />
-            <CarDetailDescription />
-            <CarDetailContact />
-        </div>
+        <CarDetailHero />
+        <CarDetailAttributes />
+        <CarDetailDescription />
+        <CarDetailContact />
     </div>
 </template>
